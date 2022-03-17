3b9a8859459078111213aa07151d8ee2843a35b6
BMWire <bmwemawire@gmail.com>
2022-03-18 00:11:25 +0300

    fix: 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,29 +1,22 @@
 #include "main.h"
 
 /**
- * print_square - print a square in the terminal
- * @size: number of times # is printed
- *
+ * print_square - prints hashes squares
+ * @size: size of the square
  * Return: void
  */
-
 void print_square(int size)
 {
 int i, j;
 
-if (size <= 0)
+for (i = 0; i < size; i++)
 {
+for (j = 0; j < size; j++)
+{
+_putchar(35);
+}
+if (i != size - 1)
 _putchar('\n');
 }
-else
-{
-for (j = 0; j < 10; j++)
-{
-for (i = 0; i < 15; i++)
-{
-_putchar('#');
-}
 _putchar('\n');
-}
-}
 }
