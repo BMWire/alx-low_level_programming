fb56396efa3d74e0c43c3e86c9e757d944b4865d
BMWire <bmwemawire@gmail.com>
2022-04-01 20:30:27 +0300

    chore: fix 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,16 +10,13 @@
  */
 int main(int argc, char *argv[])
 {
-int a, b, c;
 
-if (argc != 3)
-{
-puts("Error");
-return (1);
-}
-a = atoi(argv[1]);
-b = atoi(argv[2]);
-c = a * b;
-printf("%d\n", c);
-return (0);
+    if (argc != 3)
+    {
+        puts("Error");
+        return (1);
+    }
+
+    printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+    return (0);
 }
