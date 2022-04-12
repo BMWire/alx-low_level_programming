a8e771342065e31147304a95c367e49ccb43c464
BMWire <bmwemawire@gmail.com>
2022-04-12 05:34:39 +0300

    fix #ifndef, #define

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,7 @@
-#ifndef FUNCTION_LIKE_MACRO_H_
-#define FUNCTION_LIKE_MACRO_H_
+#ifndef _FUNCTION_LIKE_MACRO_H_
+#define _FUNCTION_LIKE_MACRO_H_
 
 #define ABS(x) ((x) < 0 ? (-(x) : (x))
 typedef int make_iso_compilers_happy;
 
-#endif /* FUNCTION_LIKE_MACRO_H_ */
+#endif /* _FUNCTION_LIKE_MACRO_H_ */
