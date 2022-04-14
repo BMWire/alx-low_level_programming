0c8ecfc17b88f9b288a29185297c6af1fa1071da
BMWire <bmwemawire@gmail.com>
2022-04-14 20:13:13 +0300

    fix: 0-sum_them_all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,23 +6,22 @@
  *
  *Return: sum of all parameters
  */
+
 int sum_them_all(const unsigned int n, ...)
 {
-int sum = 0;
-register unsigned int i;
-va_list arguments;
+	va_list valist;
+	unsigned int i;
+	int sum = 0;
 
-if (n == 0)
-{
-return (0);
-}
-else
-{
+	if (n == 0)
+		return (0);
 
-va_start(arguments, n);
-for (i = 0; i < n; i++)
-sum += va_arg(arguments, int);
-va_end(arguments);
-return (sum);
-}
+	va_start(valist, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(valist, int);
+
+	va_end(valist);
+
+	return (sum);
 }
