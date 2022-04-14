3b40b83f0d2f7e10f83e0a5f4330198cee62df92
BMWire <bmwemawire@gmail.com>
2022-04-14 20:11:06 +0300

    fix: 0-sum_them_all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,13 +8,21 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum = 0;
-	register unsigned int i;
-	va_list arguments;
+int sum = 0;
+register unsigned int i;
+va_list arguments;
 
-	va_start(arguments, n);
-	for (i = 0; i < n; i++)
-		sum += va_arg(arguments, int);
-	va_end(arguments);
-	return (sum);
+if (n == 0)
+{
+return (0);
+}
+else
+{
+
+va_start(arguments, n);
+for (i = 0; i < n; i++)
+sum += va_arg(arguments, int);
+va_end(arguments);
+return (sum);
+}
 }
